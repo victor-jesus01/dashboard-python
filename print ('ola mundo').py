281c8d00565eaf14cc18ca03a3d9937a0dd9e79b
victor-jesus01 <victorjsouza11@gmail.com>
2026-08-06 22:32:52 -0300

    first commit

---
+++ print ('ola mundo').py
@@ -0,0 +1,6 @@
+n1=int(input('largura:'))
+n2=int(input('altura:'))
+area=n1*n2
+tinta=area/2
+print('a área é: {} metros quadrados'.format(area))
+print('a quantidade de tinta necessária é: {} litros'.format(tinta))
